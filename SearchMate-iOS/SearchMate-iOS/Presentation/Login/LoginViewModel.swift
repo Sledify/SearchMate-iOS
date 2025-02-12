9a1b47019df9873b1fd85587883a82370309ccc1
kim-seonwoo <sw204011@gmail.com>
2025-02-12 15:56:24 +0900

    [Feat] Login UI

--- SearchMate-iOS/SearchMate-iOS/Presentation/Login/LoginViewModel.swift
+++ SearchMate-iOS/SearchMate-iOS/Presentation/Login/LoginViewModel.swift
@@ -5,3 +5,15 @@
 //  Created by Seonwoo Kim on 2/12/25.
 //
 
+import SwiftUI
+
+class LoginViewModel: ObservableObject {
+    @Published var email: String = ""
+    @Published var password: String = ""
+    @Published var errorMessage: String = ""
+    @Published var isLoggedIn: Bool = false
+
+    func login() {
+
+    }
+}
